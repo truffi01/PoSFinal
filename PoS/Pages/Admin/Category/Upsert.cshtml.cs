ef20eb9319a50fa79333199f8847fc79d820e675
Thomas Ruffino <thomasruffino1@gmail.com>
2020-01-09 12:21:24 -0800

    FInished Code for Upsert Page

---
+++ PoS/Pages/Admin/Category/Upsert.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PoS.DataAccess.Data.Repository.iRepository;
+
+namespace PoS
+{
+    public class UpsertModel : PageModel
+    {
+        private readonly IUnitofWork _unitOfWork;
+
+        public UpsertModel(IUnitofWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Models.Category CategoryObj { get; set; }
+
+        public IActionResult OnGet(int? id)
+        {
+            CategoryObj = new Models.Category();
+            if (id !=null)
+            {
+                CategoryObj = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id.GetValueOrDefault());
+                if (CategoryObj == null)
+                {
+                    return NotFound();
+                }
+            }
+            return Page();
+        }
+    }
+}
