593857a8492683ffaa50700a1b55e0e39f76a3e4
Thomas Ruffino <thomasruffino1@gmail.com>
2020-01-08 17:50:07 -0800

    Finished Pattern for Repository

---
+++ PoS.DataAccess/Data/Repository/CategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoS.DataAccess.Data.Repository
+{
+    public class CategoryRepository
+    {
+    }
+}
---
+++ PoS.DataAccess/Data/Repository/iRepository/IUnitofWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoS.DataAccess.Data.Repository.iRepository
+{
+    public interface IUnitofWork
+    {
+    }
+}
